edbb14a0ec0d826190ddae5f487c9ac64fb82fcc
user <user@NOTER023.tkh-staff.local>
2025-07-15 13:00:46 +0900

    Adding Sample Data

--- Docs/Docs_1.js
+++ Docs/Docs_1.js
@@ -1,11 +1,21 @@
 // William Keilsohn
 // July 7 2025
 
+// Import Packages
+const fs = require('fs');
+const csv = require('csv-parser');
+
 // Declare Variables
-var loc = process.cwd()
+var loc = process.cwd();
+var doc_loc = loc + "\\data.csv";
 
 // Declare Functions
 
+fs.createReadStream(doc_loc)
+    .pipe(csv())
+    .on('data', (row) =>{console.log(row)})
+    .on('end', () => {console.log('End of Tickets')});
+
 // Run Program
-console.log(loc);
+//console.log(doc_loc);
 
